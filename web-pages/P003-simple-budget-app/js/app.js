dd8b0f49f1b382e5a7a095ffe85b1a6ddd067737
ryuuto829 <ryuuto829@gmail.com>
2020-04-21 15:26:49 +0300

    code controllers, setup data structures and  type/read events

---
+++ web-pages/P003-simple-budget-app/js/app.js
@@ -0,0 +1,122 @@
+// BUDGET CONTROLLER
+let budgetController = (function () {
+
+    let Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    let Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    let data = {
+        allItems: {
+            inc: [],
+            exp: []
+        },
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            let newItem, id;
+
+            if (data.allItems[type].length > 0) {
+                id = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                id = 0;
+            }
+
+            if (type === 'exp') {
+                newItem = new Expense(id, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(id, des, val);
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+        testing: function () {
+            console.log(data);
+        }
+    };
+
+})();
+
+// UI CONTROLLER
+let UIController = (function () {
+
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        addListItem: function (obj, type) {
+            let html, newHtml;
+
+            if (type === 'inc') {
+                html = '<div class="budget-list__item" id="income-%id%"> <p class="budget-list__description">%description%</p> <p class="budget-list__value">%value%</p> <div class="close icon"></div> </div>';
+            } else if (type === 'exp') {
+                html = '<div class="budget-list__item" id="expense-%id%"> <p class="budget-list__description">%description%</p> <div class="right"> <p class="budget-list__value budget-list__value--left">%value%</p> <div class="budget-list__percentage">50 %</div> <div class="close icon"></div> </div> </div>';
+            }
+
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            document.querySelector('.budget-list--income').insertAdjacentHTML('beforeend', newHtml);
+
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
+
+})();
+
+// GLOBAL APP CONTROLLER
+let controller = (function (budgetCtrl, UICtrl) {
+
+    let setupEventListeners = function () {
+        let DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+
+    let ctrlAddItem = function () {
+        let input, newItem;
+
+        input = UICtrl.getInput();
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+    };
+
+    return {
+        init: function () {
+            setupEventListeners();
+        }
+    };
+
+})(budgetController, UIController);
+
+controller.init();
