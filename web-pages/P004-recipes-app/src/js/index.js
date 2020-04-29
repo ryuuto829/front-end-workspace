dae5e4e163376b7a290ef1633d5b0747d47035a7
ryuuto829 <ryuuto829@gmail.com>
2020-04-29 17:58:57 +0300

    finish building the recipe model

--- web-pages/P004-recipes-app/src/js/index.js
+++ web-pages/P004-recipes-app/src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -10,6 +11,9 @@ import { elements, renderLoader, clearLoader } from './views/base';
  */
 const state = {};
 
+/** 
+ * SEARCH CONTROLLER
+ */
 const controlSearch = async () => {
     // 1. get query from a view
     const query = searchView.getInput();
@@ -22,13 +26,18 @@ const controlSearch = async () => {
         searchView.clearInput();
         searchView.clearResults();
         renderLoader(elements.searchRes);
-        
-        // 4. Search for recipes
-        await state.search.getResults();
 
-        // 5. Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+        try {
+            // 4. Search for recipes
+            await state.search.getResults();
+    
+            // 5. Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            clearLoader();
+            alert(err);
+        }
     }
 };
 
@@ -46,3 +55,28 @@ elements.searchResPages.addEventListener('click', e => {
         searchView.renderResults(state.search.result, goToPage);
     }
 });
+
+/** 
+ * RECIPE CONTROLLER
+ */
+
+const conrolRecipe = async () => {
+    const id = window.location.hash.replace('#', '');
+
+    if (id) {
+        state.recipe = new Recipe(id);
+
+        try {
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            console.log(state.recipe);
+        } catch (err) {
+            alert(err);
+        }
+    }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, conrolRecipe));
---
+++ web-pages/P004-recipes-app/src/js/models/Recipe.js
@@ -0,0 +1,83 @@
+import axios from 'axios';
+
+export default class Recipe {
+    constructor(id) {
+        this.id = id;
+    }
+
+    async getRecipe() {
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+            this.title = res.data.recipe.title;
+            this.author = res.data.recipe.publisher;
+            this.image = res.data.recipe.image_url;
+            this.url = res.data.recipe.source_url;
+            this.ingredients = res.data.recipe.ingredients;
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    calcTime() {
+        const numIng = this.ingredients.length;
+        const periods = Math.ceil(numIng / 3);
+        this.time = periods * 15;
+    }
+
+    calcServings() {
+        this.serving = 4;
+    }
+
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoons', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+
+        const newIngredients = this.ingredients.map(el => {
+            // 1. Uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit,i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            });
+            // 2. Remove parentheses
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+            // 3. Parse ingrediens into count, unit and ingredient
+            const arrIng = ingredient.split(' ');
+            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+
+            let objIng; 
+            if (unitIndex > -1) {
+                const arrCount = arrIng.slice(0, unitIndex);
+                let count;
+
+                if (arrCount.length === 1) {
+                    count = eval(arrIng[0].replace('-','+'));
+                } else {
+                    count = eval(arrIng.slice(1, unitIndex).join('+'));
+                }
+
+                objIng = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex + 1).join(' ')
+                };
+
+            } else if (parseInt(arrIng[0], 10)) {
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ')
+                }
+
+            } else if (unitIndex === -1) {
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredient
+                }
+            }
+            return objIng;
+        });
+        this.ingredients = newIngredients;
+    }
+};
