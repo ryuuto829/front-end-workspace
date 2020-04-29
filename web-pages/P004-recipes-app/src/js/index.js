5a4095cc2fdbd2d80faec1ca6ae02a76f901a730
ryuuto829 <ryuuto829@gmail.com>
2020-04-29 14:15:04 +0300

    add search func and render results from view controller

--- web-pages/P004-recipes-app/src/js/index.js
+++ web-pages/P004-recipes-app/src/js/index.js
@@ -1,1 +1,37 @@
-console.log('start');
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
+/** GLOBAL STATE of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1. get query from a view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2. New search object and add to the state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        searchView.renderResults(state.search.result);
+    }
+};
+
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
