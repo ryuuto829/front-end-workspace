536770dc33a8cb4dda211f6204cd0ad76b1dc8c7
ryuuto829 <ryuuto829@gmail.com>
2020-05-18 12:15:32 +0300

    add desktop Toolbar navigation

--- web-pages/P005-burger-app/src/components/Layout/Layout.js
+++ web-pages/P005-burger-app/src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
+import Toolabar from '../Navigation/Toolbar/Toolbar';
 import styles from './Layout.module.css';
 
 const layout = props => (
   <Aux>
-    <div>
-      <p>Navigation Bar here ...</p>
-    </div>
+    <Toolabar />
     <main className={styles.content}>
       {props.children}
     </main>
