f7faa15a589c89e0aeedcf40b9f9107104451cd6
ryuuto829 <ryuuto829@gmail.com>
2020-05-20 18:45:17 +0300

    preparing the project for Routing

--- web-pages/P005-burger-app/src/App.js
+++ web-pages/P005-burger-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -14,9 +15,11 @@ function App() {
   const app2 = <Blog />;
 
   return (
-    <Layout>
-      {app1}
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        {app2}
+      </Layout>
+    </BrowserRouter>
   );
 }
 
---
+++ web-pages/P005-burger-app/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import axios from '../../../axios';
+import Post from '../../../components/Post/Post';
+import styles from './Posts.module.css';
+
+class Posts extends React.Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get('posts?_limit=5')
+      .then(response => {
+        const updatedPost = response.data.map(post => {
+          return {
+            ...post,
+            author: 'Dmytro'
+          }
+        });
+        this.setState({ posts: updatedPost });
+      })
+      .catch(err => {
+        console.log(err);
+        //this.setState({ error: true });
+      });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  }
+
+
+  render() {
+    let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return <Post
+          clicked={() => this.postSelectedHandler(post.id)}
+          key={post.id}
+          title={post.title}
+          author={post.author} />;
+      });
+    }
+
+    return (
+      <section className={styles.Posts}>
+        {posts}
+      </section>
+    );
+  }
+
+}
+
+export default Posts;
