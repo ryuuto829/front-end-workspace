70d2fe7b10aba80b22f0637a9ff08183cd2f51fa
ryuuto829 <ryuuto829@gmail.com>
2020-05-22 18:11:36 +0300

    add route and link switcher

--- web-pages/P005-burger-app/src/containers/Blog/Blog.js
+++ web-pages/P005-burger-app/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
 //import axios from 'axios';
 //import axios from '../../axios';
 // import Post from '../../components/Post/Post';
@@ -6,6 +7,7 @@ import React, { Component } from 'react';
 // import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render() {
@@ -14,12 +16,22 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: "/new-post",
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route
+                    path="/"
+                    exact
+                    component={Posts} />
+                <Route
+                    path="/new-post"
+                    component={NewPost} />
             </div>
         );
     }
