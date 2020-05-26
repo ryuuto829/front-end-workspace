810e262e76adf15bbc09895f847117599fbc0f1d
ryuuto829 <ryuuto829@gmail.com>
2020-05-26 17:22:58 +0300

    feat: add form validation
    Add custom form validation and create some styling for invalid values.

--- web-pages/P005-burger-app/src/containers/Checkout/ContactData/ContactData.js
+++ web-pages/P005-burger-app/src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends React.Component {
   state = {
+    formIsValid: false,
     loading: false,
     orderForm: {
       name: {
@@ -15,7 +16,12 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
@@ -23,7 +29,12 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Street'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
@@ -31,7 +42,14 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'ZIP Code'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
@@ -39,7 +57,12 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Your Country'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       email: {
         elementType: 'email',
@@ -47,7 +70,12 @@ class ContactData extends React.Component {
           type: 'text',
           placeholder: 'Your Email'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -57,18 +85,45 @@ class ContactData extends React.Component {
             { value: 'cheapest', displayValue: 'Cheapest' }
           ]
         },
-        value: ''
+        validation: {},
+        value: 'fastest',
+        valid: true
       }
     }
   }
 
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    return isValid;
+  }
+
   orderHandler = e => {
     e.preventDefault();
-
     this.setState({ loading: true });
+
+    const formData = {};
+
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
+
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      orderData: formData
     };
 
     axios.post('/orders.json', order)
@@ -82,8 +137,28 @@ class ContactData extends React.Component {
       });
   }
 
-  render() {
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormelement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
 
+    updatedFormelement.value = event.target.value;
+    updatedFormelement.valid = this.checkValidity(updatedFormelement.value, updatedFormelement.validation)
+    updatedFormelement.touched = true;
+    updatedOrderForm[inputIdentifier] = updatedFormelement;
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
+  }
+
+  render() {
     const formElementsArray = [];
 
     for (let key in this.state.orderForm) {
@@ -94,15 +169,19 @@ class ContactData extends React.Component {
     }
 
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input
             key={formElement.id}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             value={formElement.config.value}
             elementConfig={formElement.config.elementConfig}
-            elementType={formElement.config.elementType} />
+            elementType={formElement.config.elementType}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
-        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid} >ORDER</Button>
       </form>
     );
 
